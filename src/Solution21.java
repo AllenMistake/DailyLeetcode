d20b4a9a12f35e848c5204fe97dbf983e7dc6333
weitanori <weitanori@hotmail.com>
2019-02-27 20:27:26 +0800

    update Solution21 second method

--- src/Solution21.java
+++ src/Solution21.java
@@ -7,6 +7,8 @@
  * }
  */
 class Solution21 {
+
+    //自己写的
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1==null) return l2;
         if(l2==null) return l1;
@@ -29,5 +31,18 @@ class Solution21 {
         else temp.next=l1;
         return head.next;
 
+    }
+
+    //递归，来自社区大神
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
     }
 }
